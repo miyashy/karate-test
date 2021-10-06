fe56a123b9c8e35b844d83b1db8d8a24eacc6d76
miyashita-yuki1 <miyashita-yuki1@dmm.com>
2021-10-07 00:35:45 +0900

    SpringとGradle、Javaのバージョンアップ

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.3.3.RELEASE"
-    id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    kotlin("jvm") version "1.3.72"
-    kotlin("plugin.spring") version "1.3.72"
+    id("org.springframework.boot") version "2.5.5"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.5.31"
+    kotlin("plugin.spring") version "1.5.31"
 }
 
-java.sourceCompatibility = JavaVersion.VERSION_1_8
-
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
-}
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
@@ -28,12 +22,10 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-    }
-    testCompile ("com.intuit.karate:karate-junit5:0.9.6")
-    testCompile ("com.intuit.karate:karate-apache:0.9.6")
-    testCompile ("net.masterthought:cucumber-reporting:3.8.0")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation ("com.intuit.karate:karate-junit5:0.9.6")
+    testImplementation ("com.intuit.karate:karate-apache:0.9.6")
+    testImplementation ("net.masterthought:cucumber-reporting:3.8.0")
 }
 
 tasks.test {
@@ -53,7 +45,7 @@ task<Test>("parallelTest") {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
 
