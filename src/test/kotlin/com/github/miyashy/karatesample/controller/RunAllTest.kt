c1c53a9120bc7e8e4c0310489cfc3742e3ea32d8
miyashita-yuki1 <miyashita-yuki1@dmm.com>
2021-10-07 00:45:16 +0900

    Karate1.1.0でCucumberのレポート機能が不要になったため削除

--- src/test/kotlin/com/github/miyashy/karatesample/controller/RunAllTest.kt
+++ src/test/kotlin/com/github/miyashy/karatesample/controller/RunAllTest.kt
@@ -1,18 +1,13 @@
 package com.github.miyashy.karatesample.controller
 
 import com.intuit.karate.Runner
-import net.masterthought.cucumber.Configuration
-import net.masterthought.cucumber.ReportBuilder
-import org.apache.commons.io.FileUtils
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
-import java.io.File
 
 
 /**
  * 全件実行用のRunner
- * Karateのテストレポート機能は並列実行時に欠損してしまうため個別のRunnerを定義する
  */
 class RunAllTest {
 
@@ -20,11 +15,6 @@ class RunAllTest {
     @Test
     fun runAllTest() {
         val results = Runner.path("classpath:").tags("~@ignore").parallel(2)
-        val jsonFiles = FileUtils.listFiles(File(results.reportDir), arrayOf("json"), true)
-        val jsonPaths = jsonFiles.map { it.absolutePath }
-        val config = Configuration(File(results.reportDir), "demo")
-        val reportBuilder = ReportBuilder(jsonPaths, config)
-        reportBuilder.generateReports()
         Assertions.assertEquals(0, results.failCount)
     }
 }
