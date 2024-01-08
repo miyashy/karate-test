4a7db6dd508c09fbb98c25fd160b2099e6b61c51
miyashy <69844491+miyashy@users.noreply.github.com>
2024-01-08 19:00:57 +0900

    Move kotlin to java.

---
+++ src/main/java/com/github/miyashy/karatesample/KarateSampleApplication.java
@@ -0,0 +1,12 @@
+package com.github.miyashy.karatesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarateSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KarateSampleApplication.class, args);
+    }
+}
