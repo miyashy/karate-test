3aca93cd7a4c40a441878f4f45e8ae5c7114c567
miyashy <69844491+miyashy@users.noreply.github.com>
2024-01-08 19:01:26 +0900

    Add lombok.

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     integrationTestImplementation ("com.intuit.karate:karate-junit5:1.1.0")
