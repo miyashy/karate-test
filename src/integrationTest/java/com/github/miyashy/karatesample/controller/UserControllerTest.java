ec9b270e4a3a76a958baae9d97141b95d5af9547
miyashy <69844491+miyashy@users.noreply.github.com>
2024-01-08 18:39:49 +0900

    Integration test convert kotlin to java.

---
+++ src/integrationTest/java/com/github/miyashy/karatesample/controller/UserControllerTest.java
@@ -0,0 +1,15 @@
+package com.github.miyashy.karatesample.controller;
+
+import com.intuit.karate.junit5.Karate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserControllerTest {
+    @Karate.Test
+    Karate test() {
+        return Karate.run("classpath:user.feature").relativeTo(getClass());
+    }
+}
