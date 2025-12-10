91dcaadcba17a53469dea2807add3fdf5907a199
miyashy <69844491+miyashy@users.noreply.github.com>
2025-12-11 02:49:23 +0900

    chore: Spring Boot 3.3.13から3.4.7へアップデート (Phase 3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 
 plugins {
-    id("org.springframework.boot") version "3.3.13"
+    id("org.springframework.boot") version "3.4.7"
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.24"
 }
